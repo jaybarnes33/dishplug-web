0e1465fb38905c12591ab0bb7d5d184a19ac17a9
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-22 06:50:12 +0000

    Fix: Dummy food data

---
+++ src/data/foods.ts
@@ -0,0 +1,30 @@
+import { FoodType } from "@/types";
+
+const foods: FoodType[] = [
+  {
+    name: "Jollof",
+    price: 20,
+    rating: 5,
+    image: "/jollof.jpg",
+  },
+  {
+    name: "Jollof and Beef",
+    price: 20,
+    rating: 5,
+    image: "/jollofbeef.jpg",
+  },
+  {
+    name: "Assorted Fried ",
+    price: 20,
+    rating: 5,
+    image: "/friedrice.jpg",
+  },
+  {
+    name: "Pizza",
+    price: 40,
+    rating: 4,
+    image: "/pizza.jpg",
+  },
+];
+
+export default foods;
---
+++ src/data/vendors.ts
@@ -0,0 +1,30 @@
+import { VendorType } from "@/types";
+
+const vendors: VendorType[] = [
+  {
+    name: "Jollof",
+
+    rating: 5,
+    bio: "Only the best from us",
+  },
+  {
+    name: "Jollof",
+
+    rating: 5,
+    bio: "Only the best from us",
+  },
+  {
+    name: "Jollof",
+
+    rating: 5,
+    bio: "Only the best from us",
+  },
+  {
+    name: "Jollof",
+
+    rating: 5,
+    bio: "Only the best from us",
+  },
+];
+
+export default vendors;
