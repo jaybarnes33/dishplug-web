6bfaac2cc00d2f80c647d877790d836a31d2ee50
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-27 03:23:16 +0000

    Fix: Food type

--- src/data/foods.ts
+++ src/data/foods.ts
@@ -1,22 +1,22 @@
 import { FoodType } from "@/types";
 
 const foods: FoodType[] = [
-  { id: 1, name: "Jollof", price: 20, rating: 5, image: "/jollof.jpg" },
+  { id: "1", name: "Jollof", price: 20, rating: 5, image: "/jollof.jpg" },
   {
-    id: 2,
+    id: "2",
     name: "Jollof and Beef",
     price: 20,
     rating: 5,
     image: "/jollofbeef.jpg",
   },
   {
-    id: 3,
+    id: "3",
     name: "Assorted Fried ",
     price: 20,
     rating: 5,
     image: "/friedrice.jpg",
   },
-  { id: 4, name: "Pizza", price: 40, rating: 4, image: "/pizza.jpg" },
+  { id: "4", name: "Pizza", price: 40, rating: 4, image: "/pizza.jpg" },
 ];
 
 export default foods;
