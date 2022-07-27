7c0f76265c353418a7625d4d2ce725d87db429ee
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-27 16:06:49 +0000

    Fix: Constructors

--- src/helpers/constructors.ts
+++ src/helpers/constructors.ts
@@ -1,6 +1,6 @@
 export class AuthError {
-  private code: string;
-  private message: string;
+  public code: string;
+  public message: string;
 
   constructor(code: string, message: string) {
     this.code = code;
