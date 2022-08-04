50e620031a0879dda817a5f52dc82f183ad34e53
Dchole <derekoware47@gmail.com>
2022-08-04 13:03:10 +0000

    render err

--- src/pages/api/shape-shifter/cart.ts
+++ src/pages/api/shape-shifter/cart.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ message: string }>
+  res: NextApiResponse<{ message: string; err?: unknown }>
 ) {
   try {
     const db = admin.firestore();
@@ -25,6 +25,7 @@ export default async function handler(
 
     res.status(200).json({ message: "updated cart shape successfully" });
   } catch (err) {
-    res.status(500).json({ message: "Something went wrong" });
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong", err });
   }
 }
