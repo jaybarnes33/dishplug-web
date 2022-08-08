7f21a7c6800dde4a475e63557b80133d9eb01359
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-08-08 08:23:30 +0000

    Fix: Bring view all up

--- src/components/App/Main/Featured/Featured.tsx
+++ src/components/App/Main/Featured/Featured.tsx
@@ -13,6 +13,17 @@ const Featured = ({ foods }: IProps) => {
   return (
     <Container>
       <h2 className="pt-5 pb-1  text-center">Featured Food</h2>
+      <div className="d-flex justify-content-end mt-2">
+        <Link href="/foods">
+          <a
+            style={{ fontSize: "1.2rem" }}
+            className="d-flex align-items-center"
+          >
+            View all
+            <FaArrowRight className="ms-2" />
+          </a>
+        </Link>
+      </div>
       <Row className="d-flex align-items-center pt-1 ">
         {foods.map((food, index) => (
           <Col xs={6} md={4} lg={3} key={index}>
@@ -20,14 +31,6 @@ const Featured = ({ foods }: IProps) => {
           </Col>
         ))}
       </Row>
-      <div className="d-flex justify-content-end mt-2">
-        <Link href="/foods">
-          <a style={{ fontSize: "1rem" }}>
-            View all
-            <FaArrowRight />
-          </a>
-        </Link>
-      </div>
     </Container>
   );
 };
