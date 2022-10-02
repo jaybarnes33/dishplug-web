cdde59b439cc2a6a707b49172340c7126ece81e2
jaybarnes33 <57744029+jaybarnes33@users.noreply.github.com>
2022-10-02 09:01:47 +0000

    Fix: error padding

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 
 const Error = () => {
   return (
-    <div className="mt-5 pt-4 d-flex justify-content-center flex-column align-items-center">
+    <div className="mt-5 pt-5 d-flex justify-content-center flex-column align-items-center">
       <div className="my-5  d-flex justify-content-center">
         <Image width={200} height={200} alt="Error" src="/500.webp" />
       </div>
