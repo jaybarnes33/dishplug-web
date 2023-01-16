e3a61cdbcc44f7c0ad469414e5ea40da71be1cc9
Derek Oware <derekoware47@gmail.com>
2023-01-16 16:56:55 +0000

    fix; remove locationBias

--- src/components/App/Map.tsx
+++ src/components/App/Map.tsx
@@ -58,65 +58,33 @@ const Map = ({ open, handleClose }: IProps) => {
 
   useEffect(() => {
     if (map && service && !runCount.current) {
-      const infoWindow = new google.maps.InfoWindow();
+      service?.findPlaceFromQuery(
+        {
+          query: location.deliveryLocation,
+          fields: ["name", "geometry"]
+        },
+        (results, status) => {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (const result of results) {
+              const position = result.geometry?.location.toJSON();
 
-      function handleLocationError(
-        browserHasGeolocation: boolean,
-        infoWindow: google.maps.InfoWindow,
-        pos: google.maps.LatLng
-      ) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-          browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-        );
+              const marker = new google.maps.Marker({
+                position,
+                map,
+                animation: google.maps.Animation.DROP
+              });
 
-        if (map) infoWindow.open(map);
-      }
-
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          service?.findPlaceFromQuery(
-            {
-              query: location.deliveryLocation,
-              fields: ["name", "geometry"],
-              locationBias: pos
-            },
-            (results, status) => {
-              if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (const result of results) {
-                  const position = result.geometry?.location.toJSON();
-
-                  const marker = new google.maps.Marker({
-                    position,
-                    map,
-                    animation: google.maps.Animation.DROP
-                  });
-
-                  setMarker(marker);
-                  if (position) {
-                    map.setCenter(position);
-                    map.setZoom(17);
-                  }
-                }
+              setMarker(marker);
+              if (position) {
+                map.setCenter(position);
+                map.setZoom(17);
               }
             }
-          );
-
-          map.setCenter(pos);
-          runCount.current += 1;
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-        { enableHighAccuracy: true }
+          }
+        }
       );
+
+      runCount.current += 1;
     }
   }, [map, location, service]);
 
